30aba28844f20f512711f220c7ed90d604369b80
Nicholas Edwards <edwards.nicholas1997@gmail.com>
2022-11-01 10:11:35 -0400

    commit tree 32822bf4d2ffbba993b6bb88d8f9cdbd298cb5de

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 24;
+let num4 = 5;
+
+const mod = num3 % num4;
+
+let max = Math.max(11, 12, 14, 20, 19);
